9c984b1cf00a75518ccb39961e398597a96a0e9c
Fazia Batool <faziabatool28@gmail.com>
2018-02-13 21:04:57 +0000

    Homework-1

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -2,34 +2,60 @@
 
 # Part 1
 
-def sum arr
-  # YOUR CODE HERE
+ 
+def sum(array)
+	s = 0
+	array.each {|x| s= s + x}
+	s
 end
 
-def max_2_sum arr
-  # YOUR CODE HERE
+def max_2_sum(array)
+	return 0 if array.empty?
+	return array[0] if array.length == 1
+	ar = array.sort
+	ar[-1] + ar[-2]
 end
+def sum_to_n? elements, total
+  if elements.empty?
+    return false if total == 0
+  else
+    elements.combination(2).to_a.each do |pair|
+      return true if sum(pair) == total
+    end
+  end
 
-def sum_to_n? arr, n
-  # YOUR CODE HERE
+  return false
 end
 
 # Part 2
-
 def hello(name)
-  # YOUR CODE HERE
+	"Hello, #{name}"
 end
 
-def starts_with_consonant? s
-  # YOUR CODE HERE
+def starts_with_consonant? (s)
+	/^[b-df-hj-np-tv-z]/i.match(s) != nil
 end
 
-def binary_multiple_of_4? s
-  # YOUR CODE HERE
+def binary_multiple_of_4? (s)
+	return true if s == "0"
+	/^[10]*00$/.match(s) != nil
 end
 
 # Part 3
 
 class BookInStock
-# YOUR CODE HERE
+
+	attr_accessor :isbn
+	attr_accessor :price
+
+	def initialize(isbn, price)
+		raise ArgumentError, 
+			"Not a legal argument" if isbn.empty?  or price <= 0
+		@isbn = isbn
+		@price = price
+	end
+
+	def price_as_string
+		sprintf("$%2.2f", @price)
+	end
 end
